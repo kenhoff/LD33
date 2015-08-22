6ab8cc059d9cb8a813d81e0ec4f104955a1bfc1f
Ken Hoff <thekenhoff@gmail.com>
2015-08-22 14:23:24 -0700

    scene art + title fade

---
+++ Assets/Scripts/GameManager.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+import System.Collections.Generic;
+
+public var titleObject : List.<UI.Image>;
+
+public var titleFadeRate : float;
+
+private var fade : boolean;
+
+function Awake () {
+	fade = false;
+}
+
+function Update () {
+	if (Input.GetMouseButtonUp(0)) {
+		// Debug.Log("setting to fade");
+		fade = true;
+	}
+	if (fade) {
+		for (var i = 0; i < titleObject.Count; i++) {
+			titleObject[i].color = Color.Lerp(titleObject[i].color, Color.clear, Time.deltaTime);
+		}
+	}
+}
